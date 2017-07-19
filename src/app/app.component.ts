f01f4f66fffeed127a164d67fd92700f4073138a
Jesse <jesse_mclellan@yahoo.com>
2017-07-18 23:40:06 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  color = ['#ff5500', '#6600cc', '#66ff33', '#00ffcc', '#1ab2ff', '#ff1ab3', '#990099', '#cc0000', '#006600', '#ffff00'];
+
+  shuffle(a) {
+    for (let i = a.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        [a[i - 1], a[j]] = [a[j], a[i - 1]];
+    }
+  }
+
+  newArray = this.shuffle(this.color);
 }
